d0b7efc1b9419e380cf9543b620698f081714234
KAIO-BASTOS-ALVES <rm95196@estudante.fieb.edu.br>
2026-06-09 08:44:31 -0300

    Correção

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,7 @@ export default function App() {
   
   var Chance;
   const [text, setText] = useState('Brasil');
+  const [nome,setNome] = useState('Pedro Alvares Cabral');
   const [colorBck, setColorBck] = useState('white');
 
     return (
@@ -13,7 +14,14 @@ export default function App() {
 
 <Text style={[styles.title]}>Qual time vencera o hexa?</Text>
 
-    <TextInput
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o seu nome..."
+        value={nome}
+        onChangeText={(nome) => setNome(nome)}
+      />
+
+      <TextInput
         style={styles.input}
         placeholder="Digite o nome do time..."
         value={text}
@@ -22,9 +30,24 @@ export default function App() {
 
   <TouchableOpacity 
           style={styles.buttan} 
-          onPress={() => {Chance = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-                      if (Chance >= 75) {alert("O time vencedor é o: " + text); setColorBck("#edffcf")}
-                      else {alert("Não foi dessa vez :C"); setColorBck("#ffcbcb")}
+          onPress={() => {
+              Chance = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+            if(text.length > 0 && nome.length > 0 )
+              {
+                      if (Chance >= 50) {
+                        setColorBck("#edffcf");
+                        alert(nome + "! O time vencedor é o: " + text + ", Parabens!!"); 
+                      }
+                      else {
+                        setColorBck("#ffcbcb");
+                        alert(nome + ", não foi dessa vez :C"); 
+                      }
+              }
+              else {
+                alert("Preencha todos os campos!")
+                console.error("Empty input: 409")
+                setColorBck("#ffffffff");
+              }    
                       console.log(Chance)}}>
 
           <Text style={styles.buttanText}>Tentar</Text>   
@@ -52,11 +75,13 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingHorizontal: 15,
     fontSize: 16,
+    backgroundColor: '#ffffffff',
   },
   title: {
     height: 25,
-    width: 200,
-    paddingStart: 15
+    width: 300,
+    paddingStart: 35,
+    fontSize: 20
   },
   buttan: {
     backgroundColor: '#8a8a8a',
